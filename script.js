5cfe7f0adefdb41f63268557eb39142cc94a0e29
gabe fantacone <gabrielfantacone@students.berkeley.net>
2019-03-18 14:40:58 -0700

    Keep going. Change variable names and work on document.ready

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-var term = $("#artist").html();
-
-function execute(){
-    $.ajax({
-        url: "http://itunes.apple.com/search?term=" + term,
-        type: 'GET',
-        crossDomain: true,
-        dataType: "jsonp",
-        success: function(data){
-            processResults(data);
-        },
-        error: function(){
-            $("output").html("Sorry, that didn't work. Please try again.");
-        }
-    });
-}
+$(document).ready(function(){
+    function execute(){
+        var term = $("#artist").text();
+        $.ajax({
+            url: "http://itunes.apple.com/search?term=" + term,
+            type: "GET",
+            crossDomain: true,
+            dataType: "jsonp",
+            success: function(data){
+                processResults(data);
+                console.log(data);
+                console.log(data.numResults);
+            },
+            error: function(){
+                $("#output").html("Sorry, that didn't work. Please try again.");
+            }
+        });
+    }
+});
 
 function processResults(data){
     var output = $("#output").html();
-    var numResults = parseInt($("#results").find(":selected").html());
-    for (var i = 0; i < numResults; i++){
-        output += ("<td>" + i + ":" + data.result[i].artistName + "," + data.result[i].trackName +
-            "<audio controls><source src=" + data.result[i].previewUrl + "type='audio/ogg'></audio></td><td>" +
-            data.result[i].collectionName + "</td><img src=" + data.result[i].artworkUrl60 + "/>");
+    data.numResults = $("#results").val();
+    for (var i = 0; i < data.numResults; i++){
+        output += ("<td>" + i + ":" + data.results[i].artistName + "," + data.results[i].trackName +
+            "<audio controls><source src=" + data.results[i].previewUrl + "type='audio/ogg'></audio></td><td>" +
+            data.results[i].collectionName + "</td><img src=" + data.results[i].artworkUrl60 + "/>");
     }
     console.log(output);
     $("#output").html(output);
-
 }
