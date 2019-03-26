e2a15aa1bf8a3ddbffbe74b2efb3cd7e622264d1
gabe fantacone <gabrielfantacone@students.berkeley.net>
2019-03-26 14:25:37 -0700

    Keep working on 2nd page -- can we have 1 JS function?

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
             type: 'GET',
             crossDomain: true,
             dataType: 'jsonp',
-            success: function(result){
-                processResults(result);
-                return result;
+            success: function(data){
+                processResults(data);
+                return data;
             },
             error: function(){
                 $("#output").html("Sorry, that didn't work. Please try again.");
@@ -32,9 +32,41 @@ function processResults(data){
                 "'/>" + (i + 1) + ":" + data.results[i].artistName + "," + data.results[i].trackName +
                 "<audio controls><source src=" + data.results[i].previewUrl + "type='audio/m4a'></audio><br>Album:" +
                 data.results[i].collectionName +
-                "<a href='detail.html?track=" + data.results[i] + "'>See More</a></td><br>");
+                "<a href='detail.html?artist=" + data.results[i].artistName + "&track=" + data.results[i] + "'>See More</a></td><br>");
         }
         console.log(output);
         $("#output").html(output);
     }
+}
+
+function getQueryParameter(name) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0;i < vars.length;i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == name) {
+            return pair[1];
+        }
+    }
+    return false;
+}
+
+function moreStats(){
+    var artist = getQueryParameter("artist");
+    var track = getQueryParameter("track");
+    $.ajax({
+        url: "https://itunes.apple.com/search?artistName=" + artist + "&trackName=" + track,
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(data){
+            console.log(data);
+            return data;
+        },
+        error: function(){
+            $("#output").html("Sorry, that didn't work. Please try again.");
+        }
+    });
+    var output2 = $("#output2").html();
+    output2
 }
